71c1ef72e769e5b6703bc1bfa6ea29bbba9b6f86
Powie <88817288+Powie69@users.noreply.github.com>
2026-08-12 21:39:08 +0800

    update lavacastHelper

--- src/main/java/powie/powhax/modules/LavacastHelper.java
+++ src/main/java/powie/powhax/modules/LavacastHelper.java
@@ -1,7 +1,12 @@
 package powie.powhax.modules;
 
+import meteordevelopment.meteorclient.events.entity.player.InteractBlockEvent;
 import meteordevelopment.meteorclient.events.render.Render2DEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.widgets.WWidget;
+import meteordevelopment.meteorclient.gui.widgets.containers.WVerticalList;
+import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
 import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.renderer.text.TextRenderer;
 import meteordevelopment.meteorclient.settings.*;
@@ -12,14 +17,22 @@ import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.HitResult;
 import org.joml.Vector3d;
 import powie.powhax.Powhax;
 
 import java.util.Arrays;
 
 public class LavacastHelper extends Module {
-    private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
-    private final SettingGroup sgCorners = this.settings.createGroup("Corners");
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgCorners = settings.createGroup("Corners");
+
+    private final Setting<castShape> shapeMode = sgGeneral.add(new EnumSetting.Builder<castShape>()
+        .name("mode")
+        .description("get the bedrock from floor or ceiling?")
+        .defaultValue(castShape.Wedge)
+        .build()
+    );
 
     private final Setting<Integer> height = sgGeneral.add(new IntSetting.Builder()
         .name("height")
@@ -31,9 +44,16 @@ public class LavacastHelper extends Module {
     );
 
     private final Setting<Keybind> calcKeybind = sgGeneral.add(new KeybindSetting.Builder()
-        .name("keybind")
+        .name("Calculate keybind")
         .description("keybind to calculate the lavacast corners")
         .action(this::calc)
+        .build()
+    );
+
+    private final Setting<Keybind> quickSelectKeybind = sgGeneral.add(new KeybindSetting.Builder()
+        .name("Quick Select keybind")
+        .description("keybind to calculate the lavacast corners")
+        .action(this::startQuickSelecting)
         .build()
     );
 
@@ -62,7 +82,8 @@ public class LavacastHelper extends Module {
     );
 
     private BlockPos corner5 = new BlockPos(0, 0, 0);
-    private boolean shouldRender5thPoint;
+    private boolean shouldRender5thPoint, isQuickSelecting;
+    private boolean isFirstCorner = true;
 
     public LavacastHelper() {
         super(Powhax.CATEGORY, "lavacast-helper", "Helping tools for making lavacast");
@@ -71,6 +92,16 @@ public class LavacastHelper extends Module {
     @Override
     public void onDeactivate() {
         shouldRender5thPoint = false;
+    }
+
+    @Override
+    public WWidget getWidget(GuiTheme theme) {
+        WVerticalList l = theme.verticalList();
+        WButton CalcButton = l.add(theme.button("Calculate")).expandX().widget();
+        CalcButton.action = this::calc;
+        WButton quickSelectButton = l.add(theme.button("Quick Select")).expandX().widget();
+        quickSelectButton.action = this::startQuickSelecting;
+        return l;
     }
 
     @EventHandler
@@ -91,6 +122,38 @@ public class LavacastHelper extends Module {
         if (shouldRender5thPoint) renderCornerLabels(corner5, "5");
     }
 
+    @EventHandler
+    private void onInteractBlock(InteractBlockEvent event) {
+        if (!isQuickSelecting) return;
+        if (event.result.getType() == HitResult.Type.MISS) return;
+
+        if (isFirstCorner) {
+            isFirstCorner = false;
+            corner1.set(event.result.getBlockPos());
+            info("Select second corner");
+            event.cancel();
+            return;
+        }
+        corner3.set(event.result.getBlockPos());
+
+        corner2.set(new BlockPos(corner1.get().getX(), 0, corner3.get().getZ()));
+        corner4.set(new BlockPos(corner3.get().getX(), 0, corner1.get().getZ()));
+
+        calc();
+
+        info("done!");
+
+        event.cancel();
+        isFirstCorner = true;
+        isQuickSelecting = false;
+    }
+
+    private void startQuickSelecting() {
+        info("Select first corner");
+        isQuickSelecting = true;
+        mc.setScreen(null);
+    }
+
     // calc is short for calculator btw im just using slang
     private void calc() {
         shouldRender5thPoint = false;
@@ -103,17 +166,22 @@ public class LavacastHelper extends Module {
 
         corner2.set(adjustCorner(corner1.get(), corner2.get()));
         corner3.set(adjustCorner(corner2.get(), corner3.get()));
-        corner4.set(adjustCorner(corner3.get(), corner4.get()));
+        if (shapeMode.get() == castShape.Spiral) {
+            corner4.set(adjustCorner(corner3.get(), corner4.get()));
 
-        BlockPos c1 = corner1.get();
-        BlockPos c5 = adjustCorner(corner4.get(), corner1.get());
-        corner5 = new BlockPos(c1.getX(), c5.getY(), c1.getZ());
-        shouldRender5thPoint = true;
+            BlockPos c1 = corner1.get();
+            BlockPos c5 = adjustCorner(corner4.get(), corner1.get());
+            corner5 = new BlockPos(c1.getX(), c5.getY(), c1.getZ());
+            shouldRender5thPoint = true;
+        } else {
+            corner4.set(adjustCorner(corner1.get(), corner4.get()));
+        }
     }
 
-
     private BlockPos adjustCorner(BlockPos from, BlockPos to) {
-        double dist = horizontalDistance(from, to);
+        long dx = from.getX() - to.getX();
+        long dz = from.getZ() - to.getZ();
+        double dist = Math.sqrt(dx * dx + dz * dz);
         int newY = (int) Math.round(from.getY() - dist);
 
         return new BlockPos(to.getX(), newY, to.getZ());
@@ -162,12 +230,6 @@ public class LavacastHelper extends Module {
         return dx * dx + dz * dz;
     }
 
-    private double horizontalDistance(BlockPos p1, BlockPos p2) {
-        long dx = p1.getX() - p2.getX();
-        long dz = p1.getZ() - p2.getZ();
-        return Math.sqrt(dx * dx + dz * dz);
-    }
-
     private void renderCornerLabels(BlockPos corner, String label) {
         Vector3d blockPosToVec3 = new Vector3d(corner.getX() + .5, corner.getY() + .5, corner.getZ() + .5);
         if (NametagUtils.to2D(blockPosToVec3, 1.5, true)) {
@@ -177,5 +239,10 @@ public class LavacastHelper extends Module {
             TextRenderer.get().end();
             NametagUtils.end();
         }
+    }
+
+    private enum castShape {
+        Wedge,
+        Spiral,
     }
 }
